af3ae64b39d438d21bde67273b53ad77120ca1f9
dmitriy.klochkov <dmitriy.klochkov@x5.ru>
2020-09-08 18:19:48 +0400

    вывод 3 переменных

---
+++ src/Phone.java
@@ -0,0 +1,35 @@
+
+import java.io.*;
+import java. util.*;
+public class Phone {
+    public static void main(String[] args) {
+        String number = null;
+        String model = null;
+        String weight = null;
+        System.out.println(number);
+        System.out.println(model);
+        System.out.println(number);
+
+        // public Phone(String name) {
+
+        //    }
+        //   public  Phone(String number) {
+
+
+        //public Phone(String number) {
+        // }
+   //     private receiveCall (String[]){
+
+      //     String setName () {
+         //      this name = name;
+        //    }
+       //    String getNumber() {
+         //   return this.number;
+     //   }
+
+
+            System.out.println("Звонит " );
+      //  }
+
+    }
+}

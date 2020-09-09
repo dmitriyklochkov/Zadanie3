1ffd548052e21cb722bee44e9ef69e1587af09e3
dmitriy.klochkov <dmitriy.klochkov@x5.ru>
2020-09-09 12:45:16 +0400

    3 объекта phone c выводом переменных[

---
+++ Phone.java
@@ -0,0 +1,35 @@
+public class Phone {
+    public static void main(String[] args) {
+        String number = null;
+        String model = null;
+        String weight = null;
+        Phone phone = new Phone();
+        {
+            System.out.println(number);
+        }
+        Phone phone1 = new Phone();
+        {
+            System.out.println(model);
+        }
+    //    this.number = number;
+        Phone phone2 = new Phone();
+        {
+            System.out.println(weight);
+        }
+
+        //     private receiveCall (String[]){
+
+        //     String setName () {
+        //      this name = name;
+        //    }
+        //    String getNumber() {
+        //   return this.number;
+        //   }
+
+
+        System.out.println("Звонит ");
+        //  }
+
+
+    }
+}

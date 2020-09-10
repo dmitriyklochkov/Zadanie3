64636dc309afba7b496badbf3b1aef363b98767f
dmitriyklochkov <dmitriy.klochkov@x5.ru>
2020-09-10 23:29:07 +0400

    создал конструктор и метод.  не могу принять на вход name  и вывести на печать

--- Phone.java
+++ Phone.java
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 public class Phone {
 
 
     public static void main(String[] args) {
-       String number = "noy";
+        String number = "noy";
         String model = null;
         String weight = null;
         System.out.println(number);
@@ -12,13 +12,13 @@ public class Phone {
     }
 
     public Phone(String number, String model) { // конструктор
-    //   void  setNumber (String number){
-     //  this.number = number;
+        //   void  setNumber (String number){
+        //  this.number = number;
 
-       // System.out.println(number);
+        // System.out.println(number);
         //this.number = number;
 
-   }
+    }
     // System.out.println(name);
 
     //  public  Phone(String number) {
@@ -26,54 +26,15 @@ public class Phone {
 
     //public Phone(String number) {
     //
-     void receiveCall(String name) { // метод
+    void receiveCall(String name) { // метод
 
         //setName (String name) {
-          // this.name = name;
-         // }
+        // this.name = name;
+        // }
 
-        System.out.println("Звонит "+name);
+        System.out.println("Звонит " + name);
     }
     //void getNumber (String number) {
-      //  return this.number;
-  //  }
-=======
-
-public class Phone {
-    public static void main(String[] args) {
-        String number = null;
-        String model = null;
-        String weight = null;
-        Phone phone = new Phone();
-        {
-            System.out.println(number);
-        }
-        Phone phone1 = new Phone();
-        {
-            System.out.println(model);
-        }
-    //    this.number = number;
-        Phone phone2 = new Phone();
-        {
-            System.out.println(weight);
-        }
-
-       // void receiveCall (String[]){
-
-
-          public void setName (String name){
-          this.name = name;
-        System.out.println("Звонит ");
-
-         //  String getNumber() {
-      //  return this.number;
-         }
-
-
-        System.out.println("Звонит ");
-        //  }
-
-
-    }
->>>>>>> origin/master
+    //  return this.number;
+    //  }
 }

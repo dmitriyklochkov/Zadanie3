462db0f8d14eb1b33fb72b7f1c90898860255f7a
dmitriy.klochkov <dmitriy.klochkov@x5.ru>
2020-09-16 11:43:26 +0400

    создал класс анимал

--- Animal.java
+++ Animal.java
@@ -1,2 +1,19 @@
+import java.util.*;
+
 public class Animal {
+    public static void main(String[] args) {
+        String food;
+        String location;
+        String type;
+        String sound;
+    }
+         public void makeNoise (String type, String sound){
+            System.out.println(type+" животное издает звук "+sound);
+        }
+        public void eat(String type,String food){
+            System.out.println(type + " животное ест "+food);
+        }
+        public void sleep (String type,String lokation){
+            System.out.println(type + " животное спит "+lokation);
+        }
 }

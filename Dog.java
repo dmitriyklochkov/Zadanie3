a9ea19c16a74a1086e9e5c091717ce913b9dd983
dmitriy.klochkov <dmitriy.klochkov@x5.ru>
2020-09-16 12:47:16 +0400

    создал класс Dog

---
+++ Dog.java
@@ -0,0 +1,21 @@
+public class Dog extends Animal{
+   String food = "кости";
+   String location  = "в конуре";
+   String sound = "рычит";
+    String type = "собака";
+
+    @Override
+    public void eat(String type, String food) {
+        super.eat("собака", "кости");
+    }
+
+    @Override
+    public void makeNoise(String type, String sound) {
+        super.makeNoise(type, sound);
+    }
+
+    @Override
+    public void sleep(String type, String lokation) {
+        super.sleep(type, lokation);
+    }
+}
